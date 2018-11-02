4a45b0d9cea1c7cafe8af1d8fe69af228c324606
Ethan Braun <ebacdc3@gmail.com>
2018-11-02 11:16:17 -0700

    Implemented basic grid rendering

--- gridSearch.js
+++ gridSearch.js
@@ -11,9 +11,9 @@ window.onload = function(){
 	
 	function drawBackground(){
 		ctx.fillStyle = '#222';
+		ctx.shadowBlur = 0;
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 	}
-
 	
 	//Event Listeners
 	function getMousePos(e){
@@ -24,8 +24,6 @@ window.onload = function(){
 	}
 	document.addEventListener("keydown", function(e){
 		console.log("keycode: " + e.keyCode);
-		
-		
 	}, false);
 
 	document.addEventListener("mousemove", function(e){
@@ -55,9 +53,14 @@ window.onload = function(){
 		this.grid = [];
 		this.dim = 0;
 		this.canvasScale = 0.9;
+		this.squareScale = 0.9;
+		this.squareOffsetScale = (1 - this.squareScale) / 2;
 		
 		this.init = function(dim, start, end, walls){
 			this.dim = dim;
+			this.start = start;
+			this.end = end;
+			this.walls = walls;
 		};
 
 		this.update = function(canvasWidth, canvasHeight){
@@ -72,27 +75,58 @@ window.onload = function(){
 		};
 
 		this.draw = function(){
-			this.drawGridBackground();
+			//this.drawGridBackground();
 			if(this.dim !== 0){	
 				this.drawGridSquares();	
 			}
 		};
 
 		this.drawGridBackground = function(){
-			ctx.fillStyle = '#AAA';
+			ctx.fillStyle = '#888';
 			ctx.shadowColor = ctx.fillStyle;
 			ctx.shadowBlur = 10;
-			console.log(this.xOffset, this.yOffset, this.gridSize, this.gridSize);
-			ctx.rect(this.xOffset, this.yOffset, this.gridSize, this.gridSize);
-			ctx.fill();
+			ctx.fillRect(this.xOffset, this.yOffset, this.gridSize, this.gridSize);
 		};
 
 		this.drawGridSquares = function(){
-			
+			var squareSize = this.gridSize / this.dim;
+			var squareOffset = this.squareOffsetScale * squareSize;
+			var squareInnerSize = this.squareScale * squareSize;
+
+			for(var row = 0; row < this.dim; row++){
+				for(var col = 0; col < this.dim; col++){
+					if(this.start[0] === row && this.start[1] === col){
+						console.log('start: ', row, col);
+						ctx.fillStyle = '#00FF00';
+						ctx.shadowColor = ctx.fillStyle;
+						ctx.shadowBlur = 20;
+					}
+					else if(this.end[0] === row && this.end[1] === col){
+						console.log('end: ', row, col);
+						ctx.fillStyle = '#FF0000';
+						ctx.shadowColor = ctx.fillStyle;
+						ctx.shadowBlur = 20;
+					}
+					else{
+						ctx.fillStyle = '#BBB';
+						ctx.shadowColor = ctx.fillStyle;
+						ctx.shadowBlur = 0;
+					}
+					var squareX = (col * squareSize) + this.xOffset + squareOffset; 
+					var squareY = (row * squareSize) + this.yOffset + squareOffset; 
+					ctx.fillRect(squareX, squareY, squareInnerSize, squareInnerSize);
+				}
+			}
 		};
 	};
 
+	var gridDim = 20;
+	var start = [1, 0];
+	var end = [13, 18];
+	var walls = [];
+
 	var grid = new Grid();
+	grid.init(gridDim, start, end, walls);
 
 	//main functions
 
@@ -111,5 +145,5 @@ window.onload = function(){
 		draw();		
 	}
 
-	setInterval(loop, 1000/60);
+	setInterval(loop, 1000/15);
 }
