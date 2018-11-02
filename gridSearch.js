8ef7091eaf899f4cb5036c153982d2fed1108ecf
Ethan Braun <ebacdc3@gmail.com>
2018-11-02 08:25:44 -0700

    Began implementing grid prototype

--- gridSearch.js
+++ gridSearch.js
@@ -10,7 +10,7 @@ window.onload = function(){
 	resizeCanvas();
 	
 	function drawBackground(){
-		ctx.fillStyle = '#E0E0E0';
+		ctx.fillStyle = '#222';
 		ctx.fillRect(0, 0, canvas.width, canvas.height);
 	}
 
@@ -50,15 +50,60 @@ window.onload = function(){
 		canvas.height = window.innerHeight;
 	}
 
+	// Grid prototype
+	var Grid = function(){
+		this.grid = [];
+		this.dim = 0;
+		this.canvasScale = 0.9;
+		
+		this.init = function(dim, start, end, walls){
+			this.dim = dim;
+		};
+
+		this.update = function(canvasWidth, canvasHeight){
+			this.updateDrawingVars(canvasWidth, canvasHeight);
+		};
+		
+		this.updateDrawingVars = function(canvasWidth, canvasHeight){
+			var minDim = canvasWidth < canvasHeight ? canvasWidth : canvasHeight;
+			this.gridSize = minDim * this.canvasScale;
+			this.xOffset = (canvasWidth - this.gridSize) / 2;
+			this.yOffset = (canvasHeight - this.gridSize) / 2;
+		};
+
+		this.draw = function(){
+			this.drawGridBackground();
+			if(this.dim !== 0){	
+				this.drawGridSquares();	
+			}
+		};
+
+		this.drawGridBackground = function(){
+			ctx.fillStyle = '#AAA';
+			ctx.shadowColor = ctx.fillStyle;
+			ctx.shadowBlur = 10;
+			console.log(this.xOffset, this.yOffset, this.gridSize, this.gridSize);
+			ctx.rect(this.xOffset, this.yOffset, this.gridSize, this.gridSize);
+			ctx.fill();
+		};
+
+		this.drawGridSquares = function(){
+			
+		};
+	};
+
+	var grid = new Grid();
 
 	//main functions
 
 	function update(){
 		frameCount++;
+		grid.update(canvas.width, canvas.height);
 	}
 
 	function draw(){
 		drawBackground();
+		grid.draw();
 	}
 
 	function loop(){
